733d9f0f68aa15102addf422e8247b01b93d755a
TomasSG <tomassanchezgrigioni98@gmail.com>
2021-05-08 05:37:58 -0300

    Avanzando en la GUI

--- src/pk/InterfazGrafica.java
+++ src/pk/InterfazGrafica.java
@@ -23,12 +23,22 @@ import javax.swing.JList;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import javax.swing.JTextField;
 
 public class InterfazGrafica extends JFrame {
 
 	// CONSTANTES
 	private static final String TITULO = "Herramienta Testing - Grupo 03";
-	private static final String MSJ_POR_DEFECTO_LISTA_ARCHIVOS = "Aun no se encontraron archivos disponibles";
+	private static final String MSJ_LABEL_LINEAS_TOTALES_CODIGO = "Cantidad de líneas totales";
+	private static final String MSJ_LABEL_LINEAS_SOLO_CODIGO = "Cantidad de líneas de solo código";
+	private static final String MSJ_LABEL_LINEAS_COMENTADAS = "Cantidad de líneas comentadas";
+	private static final String MSJ_LABEL_LINEAS_EN_BLANCO = "Cantidad de líneas en blanco";
+	private static final String MSJ_LABEL_PORCENTAJE_DE_COMENTARIOS = "Porcentaje de comentarios (%)";
+	private static final String MSJ_LABEL_COMPLEJIDAD_CICLOMATICA = "Comlejidad ciclomatica";
+	private static final String MSJ_LABEL_FAN_IN = "Fan in";
+	private static final String MSJ_LABEL_FAN_OUT = "Fan out";
+	private static final String MSJ_LABEL_HALSTEAD_LONGITUD = "Halstead - longitud";
+	private static final String MSJ_LABEL_HALSTEAD_VOLUMEN = "Halstead - volumen";
 
 	private static final String STRING_NULA = "";
 	private static final String EXTENSION_JAVA = ".java";
@@ -49,6 +59,16 @@ public class InterfazGrafica extends JFrame {
 	// ELEMENTOS DE LA LOGICA
 	private AppManager appManager;
 	private Metodo metodo;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
 
 	/**
 	 * Launch the application.
@@ -128,7 +148,96 @@ public class InterfazGrafica extends JFrame {
 		scrollTextoCodigo.setBounds(textoCodigo.getBounds());
 		contentPane.add(scrollTextoCodigo);
 		scrollTextoCodigo.setViewportView(textoCodigo);
-		scrollTextoCodigo.setVisible(true);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 373, 124, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblLineasCodigo = new JLabel(MSJ_LABEL_LINEAS_TOTALES_CODIGO);
+		lblLineasCodigo.setBounds(10, 342, 124, 14);
+		contentPane.add(lblLineasCodigo);
+		
+		JLabel lblLineasSoloCodigo = new JLabel(MSJ_LABEL_LINEAS_SOLO_CODIGO);
+		lblLineasSoloCodigo.setBounds(180, 342, 165, 14);
+		contentPane.add(lblLineasSoloCodigo);
+		
+		JLabel lblLineasComentadas = new JLabel(MSJ_LABEL_LINEAS_COMENTADAS);
+		lblLineasComentadas.setBounds(384, 342, 165, 14);
+		contentPane.add(lblLineasComentadas);
+		
+		JLabel lblLineasBlanco = new JLabel(MSJ_LABEL_LINEAS_EN_BLANCO);
+		lblLineasBlanco.setBounds(607, 342, 153, 14);
+		contentPane.add(lblLineasBlanco);
+		
+		JLabel lblPorcentajeComentarios = new JLabel(MSJ_LABEL_PORCENTAJE_DE_COMENTARIOS);
+		lblPorcentajeComentarios.setBounds(774, 342, 153, 14);
+		contentPane.add(lblPorcentajeComentarios);
+		
+		JLabel lblComplejidadCiclomatica = new JLabel(MSJ_LABEL_COMPLEJIDAD_CICLOMATICA);
+		lblComplejidadCiclomatica.setBounds(14, 424, 117, 14);
+		contentPane.add(lblComplejidadCiclomatica);
+		
+		JLabel lblFanIn = new JLabel(MSJ_LABEL_FAN_IN);
+		lblFanIn.setBounds(443, 424, 46, 14);
+		contentPane.add(lblFanIn);
+		
+		JLabel lblFanOut = new JLabel(MSJ_LABEL_FAN_OUT);
+		lblFanOut.setBounds(239, 424, 46, 14);
+		contentPane.add(lblFanOut);
+
+		JLabel lblHalsteadLongitud = new JLabel(MSJ_LABEL_HALSTEAD_LONGITUD);
+		lblHalsteadLongitud.setBounds(630, 424, 107, 14);
+		contentPane.add(lblHalsteadLongitud);
+		
+		JLabel lblHalsteadVolumen = new JLabel(MSJ_LABEL_HALSTEAD_VOLUMEN);
+		lblHalsteadVolumen.setBounds(797, 424, 107, 14);
+		contentPane.add(lblHalsteadVolumen);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(200, 373, 124, 20);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(404, 373, 124, 20);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(621, 373, 124, 20);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(788, 373, 124, 20);
+		contentPane.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(788, 449, 124, 20);
+		contentPane.add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(621, 449, 124, 20);
+		contentPane.add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(404, 449, 124, 20);
+		contentPane.add(textField_7);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(200, 449, 124, 20);
+		contentPane.add(textField_8);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(10, 449, 124, 20);
+		contentPane.add(textField_9);
 		
 		
 		btnSeleccionarCarpeta.addActionListener(new ActionListener() {
